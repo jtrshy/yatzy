1d7309ab11c10bc8a75a8ed5038704d020d428b7
Jason Phung <jphun020@uottawa.ca>
2024-06-09 04:36:21 -0400

    Add random dice roll functionality in dice.js

---
+++ public/assets/dice.js
@@ -0,0 +1,7 @@
+function diceRoll() {
+    // let randomDiceFace = Math.floor(Math.random() * 6) + 1;
+    // console.log(randomDiceFace);
+    // return randomDiceFace;
+
+    return Math.floor(Math.random() * 6) + 1;
+}
